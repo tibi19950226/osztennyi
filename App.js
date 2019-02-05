371f256b87fb35f0855b4ce4828070a827cf847f
tibi19950226 <37515914+tibi19950226@users.noreply.github.com>
2019-02-05 15:25:15 +0100

    itt tartok
    components:form,title,weather

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class App extends React.Component{
+   render(){
+    return(
+      <div>
+        <titles />
+        <form />
+        <weather />
+      </div>
+   )
+  }
+}
+export default App;
